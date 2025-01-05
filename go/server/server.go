06106c59c88026f50cddb6b19f747b73804126b8
devLur <reiterdev@icloud.com>
2025-01-05 18:21:44 +0100

    + added go tcp server

---
+++ go/server/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+type Server struct {
+	host string
+	port string
+}
+
+func NewServer(host, port string) *Server {
+	return &Server{host: host, port: port}
+}
+
+func (s *Server) Start() {
+	listener, err := net.Listen("tcp", s.host+":"+s.port)
+	if err != nil {
+		log.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+	log.Printf("Server running on %s:%s\n", s.host, s.port)
+	fmt.Printf("Server running on %s:%s\n", s.host, s.port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+		go s.handleRequest(conn)
+	}
+}
+
+func (s *Server) handleRequest(conn net.Conn) {
+	defer conn.Close()
+
+	// Sleep to simulate some work
+	time.Sleep(1 * time.Second)
+
+	response := fmt.Sprintf("Welcome! You visited %s:%s. I did some work and returned this line to you. Time: %s\n",
+		s.host, s.port, time.Now().Format(time.RFC1123))
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		log.Println("Error writing response:", err)
+	}
+}
+
+func main() {
+	host := flag.String("host", "127.0.0.1", "IP address to bind the server")
+	port := flag.String("port", "8001", "Port to bind the server")
+	flag.Parse()
+
+	server := NewServer(*host, *port)
+	server.Start()
+}
---
+++ go/server/server_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	host := "localhost"
+	port := "8001"
+	server := NewServer(host, port)
+
+	if server.host != host {
+		t.Errorf("Expected host %s, got %s", host, server.host)
+	}
+
+	if server.port != port {
+		t.Errorf("Expected port %s, got %s", port, server.port)
+	}
+}
+
+func TestServer_Start_And_Handle_Request(t *testing.T) {
+	host := "localhost"
+	port := "8001"
+	server := NewServer(host, port)
+
+	go server.Start()
+
+	conn, err := net.Dial("tcp", host+":"+port)
+	if err != nil {
+		t.Fatalf("Failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		t.Fatalf("Failed to read from connection: %v", err)
+	}
+
+	response := string(buffer[:n])
+	expected := "Welcome! You visited " + host + ":" + port
+	if response[:len(expected)] != expected {
+		t.Errorf("Expected response to start with %s, got %s", expected, response)
+	}
+}
